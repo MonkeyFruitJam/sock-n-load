9405c058c85609691522bb31957b72ffef3cf705
Mark Ruoff <ruoff.mark@gmail.com>
2024-05-27 21:52:48 -0700

    Adding palceholder for hearts, and some pause menu functionality

--- scenes/hud.gd
+++ scenes/hud.gd
@@ -4,13 +4,26 @@ signal start_game
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
-
+	$Menu.hide()
+	$HealthHearts.hide()
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	pass
+	if Input.is_action_pressed("menu"):
+		$Menu.show();
+	if Input.is_action_pressed("close_menu"):
+		$Menu.hide();
 
 func _on_start_button_pressed():
 	$StartButton.hide()
+	$HealthHearts.show()
 	start_game.emit()
+
+
+func _on_restart_button_pressed():
+	$Menu.hide();
+	start_game.emit(); 
+
+
+func _on_quit_button_pressed():
+	get_tree().quit()
