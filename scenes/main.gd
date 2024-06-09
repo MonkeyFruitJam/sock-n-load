85ef3b77425285c86bfc53140bbf1fe284250273
Mark Ruoff <ruoff.mark@gmail.com>
2024-06-09 09:57:25 -0700

    MR-Better handling of Pause/Menu Button, adding sock heart assets

--- scenes/main.gd
+++ scenes/main.gd
@@ -1,9 +1,8 @@
 extends Node
 
-
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	$HUD/HealthHearts.hide()
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -12,4 +11,9 @@ func _process(delta):
 
 
 func _on_hud_start_game():
+	#if($HUD/StartButton.is_visible_in_tree()):
+		#$HUD/StartButton.hide()
+	if($HUD/Menu.is_visible_in_tree()):
+		$HUD/Menu.hide()
 	$HUD/HealthHearts.show()
+	# TODO: When scenes for levels get put in start first level here
