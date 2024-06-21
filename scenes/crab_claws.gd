a5fedf44efde71e95385793ef8025a880e5758ab
Gobblotamus <gobblotamus.ttv@gmail.com>
2024-06-21 22:46:03 +0200

    [Mel]Adding callback when player touch the claws

--- scenes/crab_claws.gd
+++ scenes/crab_claws.gd
@@ -2,3 +2,10 @@ extends Node2D
 
 @export var wait_time : float = 3
 @export var move_speed : float = 0.1
+@onready var test_player = owner.find_child("TestPlayer")
+
+func _on_area_2d_body_entered(body):
+	if (body == test_player):
+		print("get hit") # this should be change to make player lose health
+		# implement player get hit back, this function maybe should be in player script
+		
