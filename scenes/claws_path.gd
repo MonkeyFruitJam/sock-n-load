160a9dc8b5ed019dea1a906a3ac05f110e41bde1
Gobblotamus <gobblotamus.ttv@gmail.com>
2024-06-22 21:52:05 +0200

    [Mel] Exposing move length and create path2D through code

--- scenes/claws_path.gd
+++ scenes/claws_path.gd
@@ -1,22 +1,14 @@
-extends PathFollow2D
+extends Path2D
 
-var canMove = false
-@onready var moveTimer = $"../../MoveTimer"
-
+@onready var move_curve = Curve2D.new() 
+# Called when the node enters the scene tree for the first time.
 func _ready():
-	moveTimer.wait_time = owner.wait_time
-	moveTimer.start()
-	
-func _process(_delta):
-	var move_speed = owner.move_speed
-	if (canMove):
-		progress_ratio += _delta * move_speed
-	if (progress_ratio >= 1):
-		canMove = false
-		moveTimer.start()
-		progress_ratio = 0
-		
-func _on_timer_timeout():
-	if (!canMove):
-		canMove = true
-	moveTimer.stop()
+	var p0 = Vector2.ZERO
+	var p1 = Vector2(0,-owner.move_length)
+	print(p1)
+	var p2 = Vector2(p0)
+	move_curve.add_point(p0)
+	move_curve.add_point(p1)
+	move_curve.add_point(p2)
+	move_curve.bake_interval = 5
+	set_curve(move_curve)
--- scenes/crab_claws.gd
+++ scenes/crab_claws.gd
@@ -2,6 +2,7 @@ extends Node2D
 
 @export var wait_time : float = 3
 @export var move_speed : float = 0.1
+@export var move_length : float = 180
 @onready var test_player = owner.find_child("TestPlayer")
 
 func _on_area_2d_body_entered(body):
