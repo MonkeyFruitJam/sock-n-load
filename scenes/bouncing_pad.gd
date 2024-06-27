aba3ad9045f83c89feb6b4b9b4c80a2e5970ec1d
Gobblotamus <gobblotamus.ttv@gmail.com>
2024-06-27 21:53:02 +0200

    [Mel] expose jumping speed from the bouncing pads

--- scenes/bouncing_pad.gd
+++ scenes/bouncing_pad.gd
@@ -1,8 +1,9 @@
 extends Node2D
 
 @onready var debug_player = owner.find_child("TestPlayer")
+@export var jump_velocity = 1000
 
 func _on_area_2d_body_entered(body):
 	if (body == debug_player):
-		debug_player.velocity.y -= 1000
+		debug_player.velocity.y -= jump_velocity
 		debug_player.move_and_slide()
