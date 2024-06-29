273581370ff4e154b40c3974660c12880719aa6a
Gobblotamus <gobblotamus.ttv@gmail.com>
2024-06-29 09:50:14 +0200

    [Mel] adding check for ammunition hit

--- scenes/crab_claws.gd
+++ scenes/crab_claws.gd
@@ -4,9 +4,11 @@ extends Node2D
 @export var move_speed : float = 0.1
 @export var move_length : float = 180
 @onready var test_player = owner.find_child("TestPlayer")
+@onready var moveTimer = find_child("MoveTimer")
 
 func _on_area_2d_body_entered(body):
 	if (body == test_player):
-		print("get hit") # this should be change to make player lose health
-		# implement player get hit back, this function maybe should be in player script
+		print("hit player") # this should be change to make player lose health
+	elif (body.get_class() == "Ammunition"):
+		print("get hit, need to reset timer")
 		
