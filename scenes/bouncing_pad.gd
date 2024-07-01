5bab0a4f9cd880b56d249f4f88a274c721000f74
Gobblotamus <gobblotamus.ttv@gmail.com>
2024-07-01 22:21:52 +0200

    [Mel] Add artworks, cooldown timer and changing texture once bounce

--- scenes/bouncing_pad.gd
+++ scenes/bouncing_pad.gd
@@ -1,9 +1,25 @@
 extends Node2D
 
-@onready var debug_player = owner.find_child("TestPlayer")
-@export var jump_velocity = 1000
+#activate only for debug
+@onready var player = owner.find_child("TestPlayer")
+#@onready var player = owner.find_child("PlayerPawn")
+@export var jump_velocity : int = 1000
 
-func _on_area_2d_body_entered(body):
-	if (body == debug_player):
-		debug_player.velocity.y -= jump_velocity
-		debug_player.move_and_slide()
+@onready var cooldown_timer = find_child("CooldownTimer")
+@export var cooldown_time : float = 1
+
+var bouncing_pad_passive = preload("res://assets/bouncing_pad.png")
+var bouncing_pad_active = preload("res://assets/bouncing_pad_actived.png")
+
+func _ready():
+	cooldown_timer.wait_time = cooldown_time
+	
+func _on_bounce_area_body_entered(body):
+	if (body == player && cooldown_timer.is_stopped()):
+		player.velocity.y -= jump_velocity
+		player.move_and_slide()# Replace with function body.
+		find_child("Sprite2D").texture = bouncing_pad_active
+		cooldown_timer.start()
+
+func _on_cooldown_timer_timeout():
+	find_child("Sprite2D").texture = bouncing_pad_passive # Replace with function body.
