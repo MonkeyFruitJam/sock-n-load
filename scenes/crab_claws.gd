bf27f0cbac6bba2514a01717a8df76e85d78dd9c
Gobblotamus <gobblotamus.ttv@gmail.com>
2024-07-01 22:39:27 +0200

    [Mel] adding interactions with player and ammunition, clearing test scene

--- scenes/crab_claws.gd
+++ scenes/crab_claws.gd
@@ -3,12 +3,19 @@ extends Node2D
 @export var wait_time : float = 3
 @export var move_speed : float = 0.1
 @export var move_length : float = 180
-@onready var test_player = owner.find_child("TestPlayer")
+#@onready var player = owner.find_child("TestPlayer")
+@onready var player = owner.find_child("PlayerPawn")
 @onready var moveTimer = find_child("MoveTimer")
+var initial_pos = Vector2.ZERO
+
+func _ready():
+	initial_pos = position
 
 func _on_area_2d_body_entered(body):
-	if (body == test_player):
-		print("hit player") # this should be change to make player lose health
+	if (body == player):
+		player.get_hit()
 	elif (body.get_class() == "Ammunition"):
-		print("get hit, need to reset timer")
+		position = initial_pos
+		moveTimer.stop()
+		moveTimer.start()
 		
